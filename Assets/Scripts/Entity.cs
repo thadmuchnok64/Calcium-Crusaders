c39a4ac45a3714c4acb6da9b0f68521ccc46583a
thadmuchnok64 <58534647+thadmuchnok64@users.noreply.github.com>
2021-10-08 20:05:27 -0400

    Entity

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float maxHealth;
+    private float health;
+    [SerializeField] float speed;
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Returns the health of the entity
+    /// </summary>
+    /// <returns></returns>
+    public float GetHealth()
+    {
+        return health;
+    }
+    /// <summary>
+    /// Remove x health from the entity
+    /// </summary>
+    /// <param name="x"></param>
+    public void TakeDamage(float x)
+    {
+        health -= x;
+    }
+
+    /// <summary>
+    /// Heals entity with x health
+    /// </summary>
+    /// <param name="x"></param>
+    public void Heal(float x)
+    {
+        health += x;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+
+
+}
